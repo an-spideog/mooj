5737bf9ad8f357c1329adfea149e649340c5f1bf
killuaDev <louis.l.cullen@gmail.com>
2021-07-06 14:55:09 +0100

    Initial Commit

---
+++ Mooj/MoojApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoojApp.swift
+//  Mooj
+//
+//  Created by Leisure on 06/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct MoojApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
